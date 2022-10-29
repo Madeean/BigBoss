d46e7eaceb5513755534c3da77d8d12e7ed5f8f3
MadeReihan <reihanmade@gmail.com>
2022-10-29 17:08:11 +0700

    splash screen selesai

---
+++ app/src/main/java/umn/ac/bigboss/LoginActivity.java
@@ -0,0 +1,19 @@
+package umn.ac.bigboss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
