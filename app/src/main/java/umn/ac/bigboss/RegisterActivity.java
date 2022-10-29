f9ebb19f19dceae127ec875435d93140c08bb779
MadeReihan <reihanmade@gmail.com>
2022-10-29 21:08:06 +0700

    toolbar

--- app/src/main/java/umn/ac/bigboss/RegisterActivity.java
+++ app/src/main/java/umn/ac/bigboss/RegisterActivity.java
@@ -1,15 +1,20 @@
 package umn.ac.bigboss;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    Toolbar my_toolbar;
+    TextView my_toolbar_title;
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     ViewPagerAdapterRegister viewPagerAdapterRegister;
@@ -17,6 +22,30 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        my_toolbar = findViewById(R.id.my_toolbar);
+        my_toolbar_title = my_toolbar.findViewById(R.id.my_toolbar_title);
+        my_toolbar.setBackgroundColor(getResources().getColor(R.color.abuabumuda));
+
+
+        my_toolbar_title.setText("Register");
+        my_toolbar_title.setTextColor(getResources().getColor(R.color.hitam));
+        my_toolbar_title.setTextSize(16);
+        setSupportActionBar(my_toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        my_toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.arrow_back_hitam));
+
+        my_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inten = new Intent(RegisterActivity.this, LoginActivity.class);
+
+                startActivity(inten);
+            }
+        });
+
+
 
         tabLayout = findViewById(R.id.tab_layout);
         viewPager2 = findViewById(R.id.view_pager2);
