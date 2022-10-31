a3415aad24d412967cf68b3e1e4465be5ad028e0
MadeReihan <reihanmade@gmail.com>
2022-10-31 15:06:24 +0700

    register pengontrak

--- app/src/main/java/umn/ac/bigboss/fragmentregister/PengontrakFragment.java
+++ app/src/main/java/umn/ac/bigboss/fragmentregister/PengontrakFragment.java
@@ -7,6 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import umn.ac.bigboss.R;
 
@@ -18,6 +24,40 @@ public class PengontrakFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pengontrak, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_pengontrak, container, false);
+
+        ArrayList<String> list = new ArrayList<String>();   //make this as field atribute
+        list.add("Pilih nama kontrakan");
+        list.add("B");
+        list.add("C");
+        Spinner s = (Spinner) view.findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        s.setAdapter(adapter);
+
+
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                list.get(position);
+                if(position >0 ){
+                    Toast.makeText(getActivity(), list.get(position), Toast.LENGTH_SHORT).show();
+                }else{
+
+                    Toast.makeText(getActivity(), "Please select", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
+
+        return view;
     }
 }
