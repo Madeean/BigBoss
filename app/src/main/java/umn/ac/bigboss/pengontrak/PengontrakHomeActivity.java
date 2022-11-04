c0aeae9f9b3c1dede30d8b82834f915dbfd4317c
MadeReihan <reihanmade@gmail.com>
2022-11-04 15:08:26 +0700

    add toolbar pengontrak home

--- app/src/main/java/umn/ac/bigboss/pengontrak/PengontrakHomeActivity.java
+++ app/src/main/java/umn/ac/bigboss/pengontrak/PengontrakHomeActivity.java
@@ -1,17 +1,24 @@
 package umn.ac.bigboss.pengontrak;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import umn.ac.bigboss.R;
 
 public class PengontrakHomeActivity extends AppCompatActivity {
 
+    Toolbar my_toolbar;
+    TextView my_toolbar_title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengontrak_home);
+
+
 
     }
 }
