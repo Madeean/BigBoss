e3f85c790b0371c9e5a1e71457751baf761e38f4
MadeReihan <reihanmade@gmail.com>
2022-11-06 14:58:10 +0700

    toolbar list request pengontrak, setting sisa edit profile

--- app/src/main/java/umn/ac/bigboss/pengontrak/PengontrakListRequest.java
+++ app/src/main/java/umn/ac/bigboss/pengontrak/PengontrakListRequest.java
@@ -2,23 +2,49 @@ package umn.ac.bigboss.pengontrak;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import umn.ac.bigboss.R;
 
 
 public class PengontrakListRequest extends Fragment {
 
-
+    Toolbar my_toolbar;
+    TextView my_toolbar_title;
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String date;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pengontrak_list_request, container, false);
+        View view = inflater.inflate(R.layout.fragment_pengontrak_list_request, container, false);
+
+        my_toolbar = view.findViewById(R.id.my_toolbar_list_requerst_pengontrak);
+        my_toolbar_title = my_toolbar.findViewById(R.id.my_toolbar_title);
+        my_toolbar.setBackgroundColor(getResources().getColor(R.color.abuabumuda));
+
+
+
+        my_toolbar_title.setText("Request Pembayaran");
+        my_toolbar_title.setTextColor(getResources().getColor(R.color.hitam));
+        my_toolbar_title.setTextSize(16);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(my_toolbar);
+        activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        return view;
     }
 }
--- app/src/main/java/umn/ac/bigboss/pengontrak/PengontrakSetting.java
+++ app/src/main/java/umn/ac/bigboss/pengontrak/PengontrakSetting.java
@@ -1,23 +1,63 @@
 package umn.ac.bigboss.pengontrak;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import umn.ac.bigboss.LoginActivity;
 import umn.ac.bigboss.R;
 
 public class PengontrakSetting extends Fragment {
+    Toolbar my_toolbar;
+    TextView my_toolbar_title;
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String date;
 
+    Button btn_logout;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pengontrak_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_pengontrak_setting, container, false);
+
+        my_toolbar = view.findViewById(R.id.my_toolbar_list_requerst_pengontrak);
+        my_toolbar_title = my_toolbar.findViewById(R.id.my_toolbar_title);
+        my_toolbar.setBackgroundColor(getResources().getColor(R.color.abuabumuda));
+
+
+
+        my_toolbar_title.setText("Setting");
+        my_toolbar_title.setTextColor(getResources().getColor(R.color.hitam));
+        my_toolbar_title.setTextSize(16);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(my_toolbar);
+        activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        btn_logout = view.findViewById(R.id.btn_logout_setting_pengontrak);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
     }
 }
