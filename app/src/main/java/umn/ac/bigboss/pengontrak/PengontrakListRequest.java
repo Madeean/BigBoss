2fe6e8265e7c33bc6518d5dc313b03ffdab9ee47
MadeReihan <reihanmade@gmail.com>
2022-11-07 08:48:57 +0700

    recycle view request list pengontrak

--- app/src/main/java/umn/ac/bigboss/pengontrak/PengontrakListRequest.java
+++ app/src/main/java/umn/ac/bigboss/pengontrak/PengontrakListRequest.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +14,21 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import umn.ac.bigboss.R;
+import umn.ac.bigboss.pengontrak.adapter.AdapterDataRequestPembayaranPengontrak;
+import umn.ac.bigboss.pengontrak.adapter.adapter_data_history_pembayaran_pengontrak;
 
 
 public class PengontrakListRequest extends Fragment {
+
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+    AdapterDataRequestPembayaranPengontrak adapterData;
+    List<String> listData;
 
     Toolbar my_toolbar;
     TextView my_toolbar_title;
@@ -44,6 +55,20 @@ public class PengontrakListRequest extends Fragment {
         activity.setSupportActionBar(my_toolbar);
         activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+
+        recyclerView = view.findViewById(R.id.rv_request_pembayaran_pengontrak);
+        listData = new ArrayList<>();
+
+        for(int i = 1; i < 10; i++){
+            listData.add("madee - " + i);
+        }
+
+        linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapterData = new AdapterDataRequestPembayaranPengontrak(getActivity(), listData);
+        recyclerView.setAdapter(adapterData);
+        adapterData.notifyDataSetChanged();
 
         return view;
     }
---
+++ app/src/main/java/umn/ac/bigboss/pengontrak/adapter/AdapterDataRequestPembayaranPengontrak.java
@@ -0,0 +1,57 @@
+package umn.ac.bigboss.pengontrak.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import umn.ac.bigboss.R;
+
+public class AdapterDataRequestPembayaranPengontrak extends RecyclerView.Adapter<AdapterDataRequestPembayaranPengontrak.HolderData>{
+    List<String> listData;
+    LayoutInflater layoutInflater;
+
+
+    public AdapterDataRequestPembayaranPengontrak(Context context, List<String> listData) {
+        this.listData = listData;
+        this.layoutInflater = LayoutInflater.from(context);
+
+    }
+
+    @NonNull
+    @Override
+    public AdapterDataRequestPembayaranPengontrak.HolderData onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.from(parent.getContext()).inflate(R.layout.data_pengontrak_request_pembayaran, parent, false);
+        return new AdapterDataRequestPembayaranPengontrak.HolderData(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterDataRequestPembayaranPengontrak.HolderData holder, int position) {
+        holder.text_name_request_pengontrak.setText(listData.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    public class HolderData extends RecyclerView.ViewHolder{
+        TextView text_name_request_pengontrak,text_bulan_request_pengontrak,text_status_konfirmasi_request_pengontrak;
+
+        public HolderData(@NonNull View itemView) {
+            super(itemView);
+            text_name_request_pengontrak = itemView.findViewById(R.id.text_name_request_pengontrak);
+            text_bulan_request_pengontrak = itemView.findViewById(R.id.text_bulan_request_pengontrak);
+            text_status_konfirmasi_request_pengontrak = itemView.findViewById(R.id.text_status_konfirmasi_request_pengontrak);
+
+        }
+    }
+}
