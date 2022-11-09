f8f0bdf5c5545d764abac18d10e6c3959999bd05
MadeReihan <reihanmade@gmail.com>
2022-11-09 21:47:19 +0700

    sisa masang sidebar pada pemilik page, sambunngin intent, recycleview bagi page yg blum

---
+++ app/src/main/java/umn/ac/bigboss/pemilik/PemilikDaftarOrangNgontrak.java
@@ -0,0 +1,16 @@
+package umn.ac.bigboss.pemilik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import umn.ac.bigboss.R;
+
+public class PemilikDaftarOrangNgontrak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pemilik_daftar_orang_ngontrak);
+    }
+}
---
+++ app/src/main/java/umn/ac/bigboss/pemilik/PemilikDetailTransaksi.java
@@ -0,0 +1,16 @@
+package umn.ac.bigboss.pemilik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import umn.ac.bigboss.R;
+
+public class PemilikDetailTransaksi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pemilik_detail_transaksi);
+    }
+}
