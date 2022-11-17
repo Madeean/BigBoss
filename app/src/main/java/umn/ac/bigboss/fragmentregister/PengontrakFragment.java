ec5498ad8ee888d670f810035321ca17103efc87
MadeReihan <reihanmade@gmail.com>
2022-11-17 19:46:21 +0700

    get image from gallery atau camera di register pengontrak

--- app/src/main/java/umn/ac/bigboss/fragmentregister/PengontrakFragment.java
+++ app/src/main/java/umn/ac/bigboss/fragmentregister/PengontrakFragment.java
@@ -1,8 +1,16 @@
 package umn.ac.bigboss.fragmentregister;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +19,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -29,6 +38,11 @@ public class PengontrakFragment extends Fragment {
 
     ArrayList<String> list = new ArrayList<String>();
     String nama_kontrakan;
+    Button btn_gallery_pengontrak,btn_camera_pengontrak;
+    ImageView image_input_pengontrak;
+    int SELECT_PICTURE = 200;
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -72,7 +86,78 @@ public class PengontrakFragment extends Fragment {
 
         });
 
+        image_input_pengontrak = view.findViewById(R.id.image_input_pengontrak);
+        btn_camera_pengontrak = view.findViewById(R.id.btn_camera_pengontrak);
+        btn_gallery_pengontrak = view.findViewById(R.id.btn_gallery_pengontrak);
+        btn_gallery_pengontrak.setOnClickListener(view1 -> {
+           imageChooser();
+        });
+        btn_camera_pengontrak.setOnClickListener(view1 -> {
+            if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+            }
+            else
+            {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
+
         return view;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(getActivity(), "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+            else
+            {
+                Toast.makeText(getActivity(), "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == SELECT_PICTURE) {
+
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    image_input_pengontrak.setImageURI(selectedImageUri);
+                }
+
+        }
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            image_input_pengontrak.setImageBitmap(photo);
+        }
     }
 
 
