f2aab6939835746c750b9cec7c85c885d23cd0d5
MadeReihan <reihanmade@gmail.com>
2022-11-17 23:15:23 +0700

    get filepath gallery dan camera, sumpah ini stress bikin nya

--- app/src/main/java/umn/ac/bigboss/fragmentregister/PengontrakFragment.java
+++ app/src/main/java/umn/ac/bigboss/fragmentregister/PengontrakFragment.java
@@ -3,9 +3,13 @@ package umn.ac.bigboss.fragmentregister;
 import static android.app.Activity.RESULT_OK;
 
 import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -13,6 +17,7 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +28,10 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.util.ArrayList;
 
 import retrofit2.Call;
@@ -90,7 +99,10 @@ public class PengontrakFragment extends Fragment {
         btn_camera_pengontrak = view.findViewById(R.id.btn_camera_pengontrak);
         btn_gallery_pengontrak = view.findViewById(R.id.btn_gallery_pengontrak);
         btn_gallery_pengontrak.setOnClickListener(view1 -> {
-           imageChooser();
+            ImagePicker.Companion.with(this)
+                    .galleryOnly()	//Final image resolution will be less than 1080 x 1080(Optional)
+                    .start(SELECT_PICTURE);
+
         });
         btn_camera_pengontrak.setOnClickListener(view1 -> {
             if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
@@ -128,37 +140,70 @@ public class PengontrakFragment extends Fragment {
     }
 
 
-    void imageChooser() {
-
-        // create an instance of the
-        // intent of the type image
-        Intent i = new Intent();
-        i.setType("image/*");
-        i.setAction(Intent.ACTION_GET_CONTENT);
-
-        // pass the constant to compare it
-        // with the returned requestCode
-        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
-    }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK && requestCode == SELECT_PICTURE) {
+        if(resultCode != getActivity().RESULT_CANCELED){
+            if (resultCode == getActivity().RESULT_OK && requestCode == SELECT_PICTURE) {
+                if(data != null){
+                    Uri uri = data.getData();
+                    image_input_pengontrak.setImageURI(uri);
 
-                Uri selectedImageUri = data.getData();
-                if (null != selectedImageUri) {
-                    // update the preview image in the layout
-                    image_input_pengontrak.setImageURI(selectedImageUri);
+                    File file = ImagePicker.Companion.getFile(data);
+                    String filePath = ImagePicker.Companion.getFilePath(data);
+                    Toast.makeText(getActivity(), filePath, Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_SHORT).show();
                 }
 
+
+            }else if (requestCode == CAMERA_REQUEST && resultCode == getActivity().RESULT_OK) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                image_input_pengontrak.setImageBitmap(photo);
+
+                Uri tempUri = getImageUri(getActivity(), photo);
+                File finalFile = new File(getRealPathFromURI(tempUri));
+                Toast.makeText(getActivity(), finalFile.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_SHORT).show();
         }
-        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK)
-        {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            image_input_pengontrak.setImageBitmap(photo);
-        }
+
+
+
+
+//        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK)
+//        {
+//            Bitmap photo = (Bitmap) data.getExtras().get("data");
+////            Uri selectedImageUri = data.getData();
+//            Toast.makeText(getActivity(), photo.toString(), Toast.LENGTH_SHORT).show();
+//
+//            image_input_pengontrak.setImageBitmap(photo);
+////            image_input_pengontrak.setImageURI(selectedImageUri);
+//        }
     }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        Bitmap OutImage = Bitmap.createScaledBitmap(inImage, 1000, 1000,true);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), OutImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        String path = "";
+        if (getActivity().getContentResolver() != null) {
+            Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+                int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                path = cursor.getString(idx);
+                cursor.close();
+            }
+        }
+        return path;
+    }
+
 
 
 
